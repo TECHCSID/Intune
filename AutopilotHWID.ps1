cff8263b901c1461c26e36cad43d90d6ce4e1c75
TECHCSID <30695095+TECHCSID@users.noreply.github.com>
2023-03-28 17:02:11 +0200

    Update AutopilotHWID.ps1

--- AutopilotHWID.ps1
+++ AutopilotHWID.ps1
@@ -4,4 +4,4 @@ $env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 Install-Script -Name Get-WindowsAutopilotInfo
 Get-WindowsAutopilotInfo -OutputFile AutopilotHWID.csv
-import-csv -path "c:\hwid\autopilothwid.csv" -delimiter ","
+import-csv -path "c:\hwid\autopilothwid.csv" -delimiter "," | select-object "hardware hash"
