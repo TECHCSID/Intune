629a35c610783e59474436f943785af60250c890
TECHCSID <30695095+TECHCSID@users.noreply.github.com>
2023-03-31 14:22:56 +0200

    Update and rename AutopilotHWID.ps1 to AutopilotHWID2.ps1

--- AutopilotHWID.ps1
+++
@@ -1,7 +0,0 @@
-New-Item -Type Directory -Path “c:\HH”
-Set-Location -Path “c:\HH”
-$env:Path +=”;c:\Program Files\WindowsPowerShell\Scripts”
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
-Install-Script -Name Get-WindowsAutoPilotInfo
-Get-WindowsAutoPilotInfo -OutputFile HardwareHashDevice.csv
-import-csv -path "c:\HH\HardwareHashDevice.csv" -delimiter "," | select-object "hardware hash"
---
+++ AutopilotHWID2.ps1
@@ -0,0 +1,10 @@
+New-Item -Type Directory -Path “c:\HardHash”
+Set-Location -Path “c:\HardHash”
+$env:Path +=”;c:\Program Files\WindowsPowerShell\Scripts”
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm:$false -Force:$true
+Install-Script get-windowsautopilotinfo -Confirm:$false -Force:$true
+Install-Module -Name Microsoft.Graph.Intune -Confirm:$false -Force:$true
+Install-Module -Name WindowsAutoPilotIntune -Confirm:$false -Force:$true
+Get-WindowsAutoPilotInfo -OutputFile HardwareHashDevice.csv
+import-csv -path "c:\HardHash\HardwareHashDevice.csv" -delimiter "," | select-object "hardware hash"
