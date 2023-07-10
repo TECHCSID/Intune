67c183b1be29a2f4d2bf82f5717472b4f92ebea3
TECHCSID <30695095+TECHCSID@users.noreply.github.com>
2023-07-10 17:04:09 +0200

    Update AutopilotHWID2.ps1

--- AutopilotHWID2.ps1
+++ AutopilotHWID2.ps1
@@ -1,9 +1,8 @@
 New-Item -Type Directory -Path "c:\HardHash"
 Set-Location -Path "c:\HardHash"
 $env:Path +=";c:\Program Files\WindowsPowerShell\Scripts"
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm:$false -Force:$true
-Install-Script get-windowsautopilotinfo -Confirm:$false -Force:$true
-Install-Module -Name Microsoft.Graph.Intune -Confirm:$false -Force:$true
-Install-Module -Name WindowsAutoPilotIntune -Confirm:$false -Force:$true
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+Set-ExecutionPolicy RemoteSigned
+Install-Script -name get-windowsautopilotinfo -Confirm:$false -Force:$true
 Get-WindowsAutoPilotInfo -OutputFile HardwareHashDevice.csv
 import-csv -path "c:\HardHash\HardwareHashDevice.csv" -delimiter "," | select-object "hardware hash"
